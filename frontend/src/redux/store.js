ce293decb5c59d712116fc47d3ab361c1961bf45
Ilya Shumilov <shumilovil@gmail.com>
2021-04-04 20:23:08 +0300

    WIP

---
+++ frontend/src/redux/store.js
@@ -0,0 +1,11 @@
+import { combineReducers, createStore, applyMiddleware, compose } from "redux"
+import thunk from "redux-thunk"
+import { eatReducer } from "./testReducer"
+
+const rootReducer = combineReducers({
+    eat: eatReducer
+})
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
+export default store
---
+++ frontend/src/redux/testReducer.js
@@ -0,0 +1,34 @@
+const TOGGLE_WANT_EAT = 'TOGGLE_WANT_EAT';
+const NEW_NAME = 'NEW_NAME';
+
+const initialState = {
+    name: 'Shum',
+    wantEat: false
+};
+
+export const eatReducer = (state = initialState, action) => {
+
+    switch (action.type) {
+
+        case TOGGLE_WANT_EAT:
+            return {
+                ...state,
+                wantEat: !state.wantEat
+            }
+
+        case NEW_NAME:
+            return {
+                ...state,
+                name: action.name
+            }  
+
+        default:
+            return state;
+    };
+
+};
+
+
+
+export const toggleWantEat = () => ({ type: TOGGLE_WANT_EAT });
+export const setNewName = (payload) => ({ type: NEW_NAME, payload });
