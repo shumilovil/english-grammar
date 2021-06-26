2bf6124bf576601581835bff889589309757a639
Ilya Shumilov <shumilovil@gmail.com>
2021-06-26 13:26:52 +0300

    Achievement slider refactoring

--- frontend/src/Components/AchievementSlider/AchievementSlider.jsx
+++ frontend/src/Components/AchievementSlider/AchievementSlider.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { Image } from 'antd';
-// Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore, { Pagination, Navigation, Autoplay } from 'swiper/core';
 import { useSelector } from 'react-redux';
@@ -13,33 +12,20 @@ import './AchievementSlider.scss';
 
 
 
-// install Swiper modules
+// Install Swiper modules
 SwiperCore.use([Pagination, Navigation, Autoplay]);
-const achievementsImgs = Array.from({ length: 19 }).map((u, i) => i);
+const achievementImgs = Array.from({ length: 19 }).map((u, i) => i);
 
 export const AchievementSlider = () => {
 
-    const isMenuVisible = useSelector(state => state.app.isMenuVisible);
-    // const [autoplay, setAutoplay] = useState(false);
-    // const [activeIndexState, setActiveIndexState] = useState();
+    const isMenuVisible = useSelector(state => state.app.isMenuVisible);    
     const [isPreviewOpen, setPreviewOpen] = useState(false);
     const swiperRef = useRef(null);
-
-    // const onSlideChange = (swiper) => {
-
-    //     activeIndex.current = swiper.realIndex;
-    //     console.log('activeIndex.current', activeIndex.current);
-    // };
+    
     useEffect(() => {
         if (isMenuVisible || isPreviewOpen) {
-            // setAutoplay(false);
             swiperRef.current.swiper.autoplay.stop();
-        } else {
-            // setAutoplay({
-            //     'delay': 2500,
-            //     'pauseOnMouseEnter': true,
-            //     'disableOnInteraction': false
-            // });
+        } else {            
             swiperRef.current.swiper.autoplay.start();
         }
     }, [isMenuVisible, isPreviewOpen]);
@@ -48,9 +34,7 @@ export const AchievementSlider = () => {
         <div>
             <h2>Достижения</h2>
             <Swiper style={{ '--swiper-navigation-color': 'grey', '--swiper-pagination-color': '#1890ff' }}
-                pagination={{
-                    'clickable': true
-                }}
+                pagination={{ 'clickable': true }}
                 navigation={true}
                 loop={true}
                 slidesPerView={1}
@@ -59,40 +43,30 @@ export const AchievementSlider = () => {
                         slidesPerView: 3
                     }
                 }}
-                // onSlideChange={onSlideChange}
-                // spaceBetween={20}
                 centeredSlides={true}
-                // initialSlide={1}
                 autoplay={{
                     'delay': 3000,
-                    'pauseOnMouseEnter': true,
-                    // 'disableOnInteraction': false
+                    'pauseOnMouseEnter': true
                 }}
                 ref={swiperRef}
                 className='achievement-slider'>
 
-                {achievementsImgs.map(achievementNumber => {
+                {achievementImgs.map(achievementNumber => {
                     return (
                         <SwiperSlide key={achievementNumber}>
                             {({ isActive }) => (
                                 <div className='slide-img-wrapper'>
-                                    {
-                                        isActive
-                                            ? <Image
-                                                src={`/achievements/${achievementNumber + 1}.jpg`}
-                                                preview={{
-                                                    mask: <div>Увеличить</div>,
-                                                    onVisibleChange: (isVisible) => setPreviewOpen(isVisible)
-                                                }}
-                                            />
-                                            : <img src={`/achievements/${achievementNumber + 1}.jpg`} alt='' />
-                                    }
+                                    {isActive
+                                        ? <Image
+                                            src={`/achievements/${achievementNumber + 1}.jpg`}
+                                            preview={{
+                                                mask: <div>Увеличить</div>,
+                                                onVisibleChange: (isVisible) => setPreviewOpen(isVisible)
+                                            }}
+                                        />
+                                        : <img src={`/achievements/${achievementNumber + 1}.jpg`} alt='' />}
                                 </div>
-                            )}
-                            {/* <Image src={`/achievements/${achievementNumber + 1}.jpg`} preview={{
-                                mask: <div>Увеличить</div>,
-                                visible: isActive
-                            }} /> */}
+                            )}                            
                         </SwiperSlide>
                     );
                 })}
