a006a2854424403397b9ed7c810c19938252dabd
Ilya Shumilov <shumilovil@gmail.com>
2021-07-17 12:09:17 +0300

    cors

--- backend/app.js
+++ backend/app.js
@@ -11,10 +11,7 @@ const app = express();
 
 const PORT = config.get('port') || 5000;
 
-app.use(cors({
-    origin: process.env.NODE_ENV === 'production' ? config.get('baseUrl') : '*',
-    optionsSuccessStatus: 200
-}));
+app.use(cors());
 app.use(express.json({ extended: true }));
 app.use('/media', express.static(path.join(__dirname, 'media')));
 app.use('/api', pages);
